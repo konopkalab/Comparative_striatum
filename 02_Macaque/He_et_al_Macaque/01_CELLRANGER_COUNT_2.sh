71c884f52a9b2522d75b26f0e3d4b00c32d07295
Gozde B <65407670+g-bk@users.noreply.github.com>
2025-04-16 10:55:22 -0500

    Add files via upload

---
+++ 02_Macaque/He_et_al_Macaque/01_CELLRANGER_COUNT_2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# CREATED USING THE BIOHPC PORTAL on Fri Jun 21 2019 22:56:40 GMT-0500 (Central Daylight Time)
+#
+# This file is batch script used to run commands on the BioHPC cluster.
+# The script is submitted to the cluster using the SLURM `sbatch` command.
+# Lines starting with # are comments, and will not be run.
+# Lines starting with #SBATCH specify options for the scheduler.
+# Lines that do not start with # or #SBATCH are commands that will run.
+
+# Name for the job that will be visible in the job queue and accounting tools.
+#SBATCH --job-name count_others
+
+# Name of the SLURM partition that this job should run on.
+#SBATCH -p 256GB       # partition (queue)
+# Number of nodes required to run this job
+#SBATCH -N 1
+
+# Memory (RAM) requirement/limit in MB.
+#SBATCH --mem 254976      # Memory Requirement (MB)
+
+# Time limit for the job in the format Days-H:M:S
+# A job that reaches its time limit will be cancelled.
+# Specify an accurate time limit for efficient scheduling so your job runs promptly.
+#SBATCH -t 2-0:0:00
+
+# The standard output and errors from commands will be written to these files.
+# %j in the filename will be replace with the job number when it is submitted.
+#SBATCH -o count_others_%j.out
+#SBATCH -e count_others_%j.err
+
+module load cellranger/6.0.0
+
+cd /home2/gkonop/project/CELLRANGER_OUT/MACAQUE_HE2021
+
+refdir="/home2/gkonop/workdir/REFERENCES/cellranger_reference_RNA/rheMac10_human_liftoff/reference_genome"
+fastqdir="/home2/gkonop/project/FASTQ/MACAQUE_HE2021"
+
+# Run cellranger
+cellranger count --id=celllranger_count_SRR13808462 --transcriptome=$refdir --fastqs=$fastqdir --sample=SRR13808462 --include-introns
+
+
+
+
+
+
+
+
+
+
+
