6716d3d8b471b6e2e33503520b4f4c1d506b3760
Gozde B <65407670+g-bk@users.noreply.github.com>
2025-04-17 14:13:08 -0500

    Update 02_CELLBENDER.sh

--- 02_Macaque/02_CELLBENDER.sh
+++ 02_Macaque/02_CELLBENDER.sh
@@ -5,8 +5,6 @@ module load python/3.8.x-anaconda
 module load gcc/12.2.0
 source activate cellbender
 
-cd /home2/gkonop/project/03_MATRIX_FROM_CELLBENDER/HUMAN_OUR/CELLBENDER_RUN
-
 for dir in `ls -d $PWD/*`
 do
 
