5c6ec83159cf4626a297701cfd98b98faf26e84b
Gozde B <65407670+g-bk@users.noreply.github.com>
2026-01-09 14:36:10 -0800

    Update 04_Correlation_withinBatPutamen.R

--- 04_Interneurons/04_Correlation_withinBatPutamen.R
+++ 04_Interneurons/04_Correlation_withinBatPutamen.R
@@ -87,3 +87,18 @@ pheatmap(correlation_matrix, cluster_rows = TRUE, cluster_cols = TRUE,
          show_rownames = TRUE, show_colnames = TRUE, 
          main = "Cluster-wise Gene Expression Correlation")
 dev.off()
+
+correlation_matrix["LMO3_BAT", "FOXP2_EYA2"]
+correlation_matrix["LMO3_BAT", "FOXP2_TSHZ2_BAT"]
+correlation_matrix["FOXP2_EYA2", "TH"]
+correlation_matrix["FOXP2_TSHZ2_BAT", "TAC3"]
+[1] 0.9578292
+[1] 0.9276468
+[1] 0.94205
+[1] 0.8972202
+correlation_matrix[c("LMO3_BAT","FOXP2_EYA2","FOXP2_TSHZ2_BAT"),
+        c("TH","TAC3")]
+                       TH      TAC3
+LMO3_BAT        0.9258643 0.9142284
+FOXP2_EYA2      0.9420500 0.9248304
+FOXP2_TSHZ2_BAT 0.9089461 0.8972202
